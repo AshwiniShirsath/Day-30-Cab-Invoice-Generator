5bc0e520befb0beb70703405fd70fb54f8077d8e
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-05-11 13:02:56 +0530

    [ADDED] : Cab Invoice Generator With Premium and Normal Ride

---
+++ src/com/bridgelabz/cabvoicegenerator/Rides.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.cabvoicegenerator;
+
+public class Rides {
+	int distance;
+    int duration;
+    enum RideType{
+        NORMAL,PREMIUM
+    };
+    public Rides(int distance, int duration) {
+        this.distance = distance;
+        this.duration = duration;
+    }
+
+}
+
+
